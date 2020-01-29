8f01db0dd4cbfc8a761dc8dd3701a8a36bd0b968
David Miranda Grinstein <davidmirandagrinstein@gmail.com>
2020-01-29 02:27:48 -0300

    rsvp modal?

--- src/screens/report.js
+++ src/screens/report.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Card, Image, Icon, Button } from 'semantic-ui-react'
+import { Container, Card, Image, Icon, Button, Modal, Step } from 'semantic-ui-react'
 import CampaignService from '../services/campaign'
 
 function ReportScreen({ match, history, openLoadingModal, closeLoadingModal }) {
@@ -7,6 +7,23 @@ function ReportScreen({ match, history, openLoadingModal, closeLoadingModal }) {
         flyer: '',
         rsvp: []
     })
+    const [rsvpModal, setRsvpModal] = useState({
+        visible: false
+    })
+
+    function openRsvpModal() {
+        setRsvpModal({
+            ...rsvpModal,
+            visible: true
+        })
+    }
+
+    function closeRsvpModal() {
+        setRsvpModal({
+            ...rsvpModal,
+            visible: false
+        })
+    }
 
     useEffect(() => {
         async function fetchReport() {
@@ -26,39 +43,87 @@ function ReportScreen({ match, history, openLoadingModal, closeLoadingModal }) {
 
     return (
         <>
-            <Container style={{ height: '80vh' }}>
-                <Card
-                    style={{ marginTop: '8vh' }}
-                    centered
-                >
-                    <Image src={reportState.flyer.replace('150x100', '600x400')} wrapped ui={false} />
-                    <Card.Content>
-                        <Card.Header>{reportState.title}</Card.Header>
-                        <Card.Meta>
-                            <p className='date'>{reportState.date}</p>
-                            <p className='date'>{reportState.start_time} / {reportState.end_time}</p>
-                        </Card.Meta>
-                        <Card.Description>
-                            {reportState.info}
-                        </Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <Button
-                            icon='send'
-                            circular
-                            floated='left'
-                            content={reportState.rsvp.length}
-                        />
+            {
+                rsvpModal.visible && (
+                    <RsvpModal
+                        visible={rsvpModal.visible}
+                        campaign={reportState.campaign}
+                        onClose={closeRsvpModal}
+                    />
+                )
+            }
 
-                        <Button
-                            icon='add user'
-                            circular
-                            floated='right'
-                        />
-                    </Card.Content>
-                </Card>
+            <Container style={{ height: '80vh' }}>
+                {
+                    reportState.id && (
+                        <Card
+                            style={{ marginTop: '8vh' }}
+                            centered
+                        >
+                            <Image src={reportState.flyer.replace('150x100', '600x400')} wrapped ui={false} />
+                            <Card.Content>
+                                <Card.Header>{reportState.title}</Card.Header>
+                                <Card.Meta>
+                                    <p className='date'>{reportState.date}</p>
+                                    <p className='date'>{reportState.start_time} / {reportState.end_time}</p>
+                                </Card.Meta>
+                                <Card.Description>
+                                    {reportState.info}
+                                </Card.Description>
+                            </Card.Content>
+                            <Card.Content extra>
+                                <Button.Group floated='left'>
+                                    <Button labelPosition='left' icon='female' content={reportState.stats.female_count} />
+                                    <Button labelPosition='right' icon='male' content={reportState.stats.male_count} />
+                                </Button.Group>
+
+                                <Button
+                                    icon='add user'
+                                    circular
+                                    floated='right'
+                                    onClick={openRsvpModal}
+                                />
+                            </Card.Content>
+                        </Card>
+                    )
+                }
             </Container>
         </>
+    )
+}
+
+function RsvpModal({ visible, campaign, onClose }) {
+    const [stepIndex, setStepIndex] = useState(0)
+
+    return (
+        <Modal
+            dimmer="inverted"
+            open={visible}
+            onClose={onClose}
+            size='large'
+        >
+            <Modal.Content>
+                <Step.Group attached='top' fluid unstackable>
+                    <Step active={stepIndex === 0}>
+                        <Icon name='phone' />
+                        <Step.Content>
+                            <Step.Title>Phone number</Step.Title>
+                            <Step.Description></Step.Description>
+                        </Step.Content>
+                    </Step>
+
+                    <Step active={stepIndex === 1}>
+                        <Icon name='user' />
+                        <Step.Content>
+                            <Step.Title>Guest</Step.Title>
+                            <Step.Description>Enter guest information</Step.Description>
+                        </Step.Content>
+                    </Step>
+                </Step.Group>
+
+
+            </Modal.Content>
+        </Modal>
     )
 }
 
